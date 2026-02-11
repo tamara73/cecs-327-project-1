916460ceb26e1c5a233e0c497e13a8dcbd58d2b4
Melanie Cativo <melaniecativo@Mac.lan>
2026-02-10 23:40:33 -0800

    Creating socket outline, currently testing not finalized for project.

--- app_server.py
+++ app_server.py
@@ -1,1 +1,17 @@
-#testing
+import socket
+
+# gets ip address from local computer (might need to replace later)
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+while True:
+    comm_socket, address = server.accept()
+    print(f"Connected to {address}!")
+    message = comm_socket.recv(1024).decode('utf-8')
+    print(f"Message from client is: {message}")
+    comm_socket.send(f"Message received.").encode('utf-8')
